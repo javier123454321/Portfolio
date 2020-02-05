b01da3ffa6843a728e8e2a392ce7bd7b35d2786b
Javier Gonzalez <Hal@Dr-Jeckyll.lan>
2020-02-04 22:18:29 -0600

    Form Stuffs

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="icon" href="/img/favicon.png" type="image/png">
-	<title>Javier Gonzalez | Software Developer, Designer, Amateur</title>
+	<title>Thank You for Contacting Me.</title>
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/style.css">
@@ -16,7 +16,39 @@
 			<div class="container">
 				<div class="row d-flex text-center align-items-center">
 					<div class="col-lg-12 tagLine">
-						<h1>Thank You, I will get back to you very soon!</h1>
+<?php
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $visitor_message = "";
+
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+
+    if(isset($_POST['message'])) {
+        $visitor_message = "From: $visitor_name.<br><br>" ."Body: <br><br>" .wordwrap(htmlspecialchars($_POST['message']), 70);
+    }
+
+    $mailTo = "javier.e.glz@gmail.com";
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+	.'From: ' . $visitor_email . "\r\n";
+	
+    if(mail($mailTo, $email_title, $visitor_message, $headers)){
+		echo "<h1>Thank you, I will get back to you very soon!</h1>";
+   	 } else {
+	  echo "<h1> This is awkward... something went wrong</h1> <br>
+	   		<h3>please contact me at javier.e.glz@gmail.com</h3>";
+    }
+?>
+						
 						<div class="ctaHire">
 							<a href="/" class="btn circle">Go back</a>
 						</div>
@@ -25,34 +57,3 @@
 			</div>
 		</section>
 	</body>
-
-
-<?php
-    $to = "javier.e.glz@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $cmessage = $_REQUEST['message'];
-
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-    $subject = "Contact from Grasping Design";
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-?>
